c6261b97049c58eea08d072fea38f0ec2a1aeef6
Nicolas Coderre <ncoderre@zynga.com>
2017-04-27 15:49:53 -0700

    rotate script

--- test.js
+++ test.js
@@ -1,5 +1,23 @@
-var Rotate = pc.createScript("rotate");
+/*var Rotate = pc.createScript("rotate");
 
 Rotate.prototype.update = function (dt) {
     this.entity.rotate(0, 10*dt, 0);
 };
+*/
+pc.script.create("rotate", function (app) {
+    var ScriptObject = function (entity) {
+        this.entity = entity;
+    };
+
+    ScriptObject.prototype = {
+        initialize: function () {
+
+        },
+
+        update: function (dt) {
+            this.entity.rotate(0, 10*dt, 0);
+        }
+    };
+
+    return ScriptObject;
+});
